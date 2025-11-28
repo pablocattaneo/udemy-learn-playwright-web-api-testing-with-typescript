9cec7ba4968a7a086b65eff88a0c5e57f61159e8
Pablo Cattaneo <pablocattaneo1@gmail.com>
2025-11-28 09:51:32 -0300

    Add test for visibility of Remember Me checkbox

--- tests/section-3/lesson-19-locators/lesson-19-locators.spec.ts
+++ tests/section-3/lesson-19-locators/lesson-19-locators.spec.ts
@@ -1,14 +1,27 @@
 import { expect, test } from "@playwright/test";
 
 test("Primary Action is visible", async ({ page }) => {
-  await page.goto("http://localhost:8080/section-3/lesson-19-locators/app.html");
+  await page.goto(
+    "http://localhost:8080/section-3/lesson-19-locators/app.html"
+  );
 
   const primaryButton = page.getByRole("button", { name: "Primary Action" });
   await expect(primaryButton).toBeVisible();
 });
 test("Username input is visible", async ({ page }) => {
-  await page.goto("http://localhost:8080/section-3/lesson-19-locators/app.html");
+  await page.goto(
+    "http://localhost:8080/section-3/lesson-19-locators/app.html"
+  );
 
   const usernameInput = page.getByRole("textbox", { name: "Username" });
   await expect(usernameInput).toBeVisible();
+});
+test("checkbox   is visible", async ({ page }) => {
+  await page.goto(
+    "http://localhost:8080/section-3/lesson-19-locators/app.html"
+  );
+  const rememberMeCheckbox = page.getByRole("checkbox", {
+    name: " Accept terms",
+  });
+  await expect(rememberMeCheckbox).toBeVisible();
 });
