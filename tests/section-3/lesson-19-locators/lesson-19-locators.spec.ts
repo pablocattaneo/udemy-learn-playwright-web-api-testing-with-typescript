f261868a5fef20e5fb62958d5dd37ca2a3546a40
Pablo Cattaneo <pablocattaneo1@gmail.com>
2025-11-28 09:47:16 -0300

    Add test for visibility of Username input

--- tests/section-3/lesson-19-locators/lesson-19-locators.spec.ts
+++ tests/section-3/lesson-19-locators/lesson-19-locators.spec.ts
@@ -6,3 +6,9 @@ test("Primary Action is visible", async ({ page }) => {
   const primaryButton = page.getByRole("button", { name: "Primary Action" });
   await expect(primaryButton).toBeVisible();
 });
+test("Username input is visible", async ({ page }) => {
+  await page.goto("http://localhost:8080/section-3/lesson-19-locators/app.html");
+
+  const usernameInput = page.getByRole("textbox", { name: "Username" });
+  await expect(usernameInput).toBeVisible();
+});
