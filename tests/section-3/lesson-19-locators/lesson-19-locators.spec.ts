0ec848a2b85b51aadf12dd84fc5fb33dd680165b
Pablo Cattaneo <pablocattaneo1@gmail.com>
2025-12-01 08:50:03 -0300

    Refactor tests to use BASE_URL constant for navigation and add tests for elements by data-test-id attribute

--- tests/section-3/lesson-19-locators/lesson-19-locators.spec.ts
+++ tests/section-3/lesson-19-locators/lesson-19-locators.spec.ts
@@ -1,28 +1,24 @@
 import { expect, test } from "@playwright/test";
 import { describe } from "node:test";
 
+const BASE_URL = "http://localhost:8080/section-3/lesson-19-locators/app.html";
+
 // Lesson 19: Locators
 describe("Lesson 19: Locators: Locate elements by their explicit or implicit ARIA roles.", () => {
   test("Primary Action is visible", async ({ page }) => {
-    await page.goto(
-      "http://localhost:8080/section-3/lesson-19-locators/app.html"
-    );
+    await page.goto(BASE_URL);
 
     const primaryButton = page.getByRole("button", { name: "Primary Action" });
     await expect(primaryButton).toBeVisible();
   });
   test("Username input is visible", async ({ page }) => {
-    await page.goto(
-      "http://localhost:8080/section-3/lesson-19-locators/app.html"
-    );
+    await page.goto(BASE_URL);
 
     const usernameInput = page.getByRole("textbox", { name: "Username" });
     await expect(usernameInput).toBeVisible();
   });
   test("checkbox   is visible", async ({ page }) => {
-    await page.goto(
-      "http://localhost:8080/section-3/lesson-19-locators/app.html"
-    );
+    await page.goto(BASE_URL);
     const rememberMeCheckbox = page.getByRole("checkbox", {
       name: " Accept terms",
     });
@@ -34,9 +30,7 @@ describe("Lesson 19: Locators: Locate elements by their text content.", () => {
   test("This paragraph contains some important text that you might want to locate. is visible", async ({
     page,
   }) => {
-    await page.goto(
-      "http://localhost:8080/section-3/lesson-19-locators/app.html"
-    );
+    await page.goto(BASE_URL);
 
     const importantText = page.getByText(
       "This paragraph contains some important text that you might want to locate."
@@ -44,18 +38,14 @@ describe("Lesson 19: Locators: Locate elements by their text content.", () => {
     await expect(importantText).toBeVisible();
   });
   test("Special: Unique text identifier is visible", async ({ page }) => {
-    await page.goto(
-      "http://localhost:8080/section-3/lesson-19-locators/app.html"
-    );
+    await page.goto(BASE_URL);
     const uniqueText = page.getByText("Special: Unique text identifier");
     await expect(uniqueText).toBeVisible();
   });
   test("Click the button above to submit your information. is visible", async ({
     page,
   }) => {
-    await page.goto(
-      "http://localhost:8080/section-3/lesson-19-locators/app.html"
-    );
+    await page.goto(BASE_URL);
     const buttonInstructionText = page.getByText(
       "Click the button above to submit your information."
     );
@@ -65,39 +55,29 @@ describe("Lesson 19: Locators: Locate elements by their text content.", () => {
 
 describe("Lesson 19: Locators: Locate form controls by their associated label text.", () => {
   test("Email Address: label is visible", async ({ page }) => {
-    await page.goto(
-      "http://localhost:8080/section-3/lesson-19-locators/app.html"
-    );
+    await page.goto(BASE_URL);
 
     const emailLabel = page.getByLabel("Email Address:");
     await expect(emailLabel).toBeVisible();
   });
   test("Password: label is visible", async ({ page }) => {
-    await page.goto(
-      "http://localhost:8080/section-3/lesson-19-locators/app.html"
-    );
+    await page.goto(BASE_URL);
 
     const passwordLabel = page.getByLabel("Password:");
     await expect(passwordLabel).toBeVisible();
   });
   test("Your Age: label is visible", async ({ page }) => {
-    await page.goto(
-      "http://localhost:8080/section-3/lesson-19-locators/app.html"
-    );
+    await page.goto(BASE_URL);
     const ageLabel = page.getByLabel("Your Age:");
     await expect(ageLabel).toBeVisible();
   });
   test("Standard: label is visible", async ({ page }) => {
-    await page.goto(
-      "http://localhost:8080/section-3/lesson-19-locators/app.html"
-    );
+    await page.goto(BASE_URL);
     const standardLabel = page.getByLabel("Standard");
     await expect(standardLabel).toBeVisible();
   });
   test(" Express: label is visible", async ({ page }) => {
-    await page.goto(
-      "http://localhost:8080/section-3/lesson-19-locators/app.html"
-    );
+    await page.goto(BASE_URL);
     const expressLabel = page.getByLabel("Express");
     await expect(expressLabel).toBeVisible();
   });
@@ -105,9 +85,7 @@ describe("Lesson 19: Locators: Locate form controls by their associated label te
 
 describe("Lesson 19: Locators: Locate input elements by their placeholder text", () => {
   test("Enter your full name placeholder is visible", async ({ page }) => {
-    await page.goto(
-      "http://localhost:8080/section-3/lesson-19-locators/app.html"
-    );
+    await page.goto(BASE_URL);
 
     const fullNamePlaceholder = page.getByPlaceholder("Enter your full name");
     await expect(fullNamePlaceholder).toBeVisible();
@@ -115,9 +93,7 @@ describe("Lesson 19: Locators: Locate input elements by their placeholder text",
   test(" Phone number (xxx-xxx-xxxx) placeholder is visible", async ({
     page,
   }) => {
-    await page.goto(
-      "http://localhost:8080/section-3/lesson-19-locators/app.html"
-    );
+    await page.goto(BASE_URL);
 
     const phonePlaceholder = page.getByPlaceholder(
       "Phone number (xxx-xxx-xxxx)"
@@ -125,9 +101,7 @@ describe("Lesson 19: Locators: Locate input elements by their placeholder text",
     await expect(phonePlaceholder).toBeVisible();
   });
   test("Type your message here... placeholder is visible", async ({ page }) => {
-    await page.goto(
-      "http://localhost:8080/section-3/lesson-19-locators/app.html"
-    );
+    await page.goto(BASE_URL);
 
     const messagePlaceholder = page.getByPlaceholder(
       "Type your message here..."
@@ -135,9 +109,7 @@ describe("Lesson 19: Locators: Locate input elements by their placeholder text",
     await expect(messagePlaceholder).toBeVisible();
   });
   test("Search products... placeholder is visible", async ({ page }) => {
-    await page.goto(
-      "http://localhost:8080/section-3/lesson-19-locators/app.html"
-    );
+    await page.goto(BASE_URL);
     const searchPlaceholder = page.getByPlaceholder("Search products...");
     await expect(searchPlaceholder).toBeVisible();
   });
@@ -145,9 +117,7 @@ describe("Lesson 19: Locators: Locate input elements by their placeholder text",
 
 describe("Lesson 19: Locators: Locate elements (usually images) by their alt text.", () => {
   test("Logo image with alt text logo image is visible", async ({ page }) => {
-    await page.goto(
-      "http://localhost:8080/section-3/lesson-19-locators/app.html"
-    );
+    await page.goto(BASE_URL);
 
     const logoImage = page.getByAltText("logo image");
     await expect(logoImage).toBeVisible();
@@ -156,25 +126,60 @@ describe("Lesson 19: Locators: Locate elements (usually images) by their alt tex
 
 describe("Lesson 19: Locators: Locate elements by their title attribute.", () => {
   test("Element with title Home page link is visible", async ({ page }) => {
-    await page.goto(
-      "http://localhost:8080/section-3/lesson-19-locators/app.html"
-    );
+    await page.goto(BASE_URL);
 
-    const  htmlTitleElement = page.getByTitle("Home page link");
+    const htmlTitleElement = page.getByTitle("Home page link");
     await expect(htmlTitleElement).toBeVisible();
   });
-  test(" Element with title HyperText Markup Language is visible", async ({ page }) => {
-    await page.goto(
-      "http://localhost:8080/section-3/lesson-19-locators/app.html"
-    );
+  test(" Element with title HyperText Markup Language is visible", async ({
+    page,
+  }) => {
+    await page.goto(BASE_URL);
     const htmlTitleElement = page.getByTitle("HyperText Markup Language");
     await expect(htmlTitleElement).toBeVisible();
   });
   test(" Element with title Tooltip text is visible", async ({ page }) => {
-    await page.goto(
-      "http://localhost:8080/section-3/lesson-19-locators/app.html"
-    );
+    await page.goto(BASE_URL);
     const tooltipTitleElement = page.getByTitle("Tooltip text");
     await expect(tooltipTitleElement).toBeVisible();
+  });
+});
+
+describe("Lesson 19: Locators: Locate elements by their data-test-id attribute.", () => {
+  test(" Element with data-test-id user-profile-card is visible", async ({
+    page,
+  }) => {
+    await page.goto(BASE_URL);
+
+    const userProfileCard = page.getByTestId("user-profile-card");
+    await expect(userProfileCard).toBeVisible();
+  });
+  test(" Element with data-test-id profile-name is visible", async ({
+    page,
+  }) => {
+    await page.goto(BASE_URL);
+    const profileName = page.getByTestId("profile-name");
+    await expect(profileName).toBeVisible();
+  });
+  test(" Element with data-test-id profile-email is visible", async ({
+    page,
+  }) => {
+    await page.goto(BASE_URL);
+    const profileEmail = page.getByTestId("profile-email");
+    await expect(profileEmail).toBeVisible();
+  });
+  test(" Element with data-test-id edit-profile-btn is visible", async ({
+    page,
+  }) => {
+    await page.goto(BASE_URL);
+    const editProfileBtn = page.getByTestId("edit-profile-btn");
+    await expect(editProfileBtn).toBeVisible();
+  });
+  test(" Element with data-test-id product-grid is visible", async ({
+    page,
+  }) => {
+    await page.goto(BASE_URL);
+    const productGrid = page.getByTestId("product-grid");
+    await expect(productGrid).toBeVisible();
   });
 });
