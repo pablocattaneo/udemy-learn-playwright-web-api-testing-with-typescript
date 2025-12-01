42c9f37ccc8c83fbe9bf44c6fb024b3a0d895c8f
Pablo Cattaneo <pablocattaneo1@gmail.com>
2025-12-01 08:24:23 -0300

    IT adds test for ARIA roles exercises

--- tests/section-3/lesson-19-locators/lesson-19-locators.spec.ts
+++ tests/section-3/lesson-19-locators/lesson-19-locators.spec.ts
@@ -1,27 +1,46 @@
 import { expect, test } from "@playwright/test";
+import { describe } from "node:test";
 
-test("Primary Action is visible", async ({ page }) => {
-  await page.goto(
-    "http://localhost:8080/section-3/lesson-19-locators/app.html"
-  );
+// Lesson 19: Locators
+describe("Lesson 19: Locators: Locate elements by their explicit or implicit ARIA roles.", () => {
+  test("Primary Action is visible", async ({ page }) => {
+    await page.goto(
+      "http://localhost:8080/section-3/lesson-19-locators/app.html"
+    );
 
-  const primaryButton = page.getByRole("button", { name: "Primary Action" });
-  await expect(primaryButton).toBeVisible();
-});
-test("Username input is visible", async ({ page }) => {
-  await page.goto(
-    "http://localhost:8080/section-3/lesson-19-locators/app.html"
-  );
-
-  const usernameInput = page.getByRole("textbox", { name: "Username" });
-  await expect(usernameInput).toBeVisible();
-});
-test("checkbox   is visible", async ({ page }) => {
-  await page.goto(
-    "http://localhost:8080/section-3/lesson-19-locators/app.html"
-  );
-  const rememberMeCheckbox = page.getByRole("checkbox", {
-    name: " Accept terms",
+    const primaryButton = page.getByRole("button", { name: "Primary Action" });
+    await expect(primaryButton).toBeVisible();
   });
-  await expect(rememberMeCheckbox).toBeVisible();
+  test("Username input is visible", async ({ page }) => {
+    await page.goto(
+      "http://localhost:8080/section-3/lesson-19-locators/app.html"
+    );
+
+    const usernameInput = page.getByRole("textbox", { name: "Username" });
+    await expect(usernameInput).toBeVisible();
+  });
+  test("checkbox   is visible", async ({ page }) => {
+    await page.goto(
+      "http://localhost:8080/section-3/lesson-19-locators/app.html"
+    );
+    const rememberMeCheckbox = page.getByRole("checkbox", {
+      name: " Accept terms",
+    });
+    await expect(rememberMeCheckbox).toBeVisible();
+  });
+});
+
+describe("Lesson 19: Locators: Locate elements by their text content.", () => {
+  test("This paragraph contains some important text that you might want to locate. is visible", async ({
+    page,
+  }) => {
+    await page.goto(
+      "http://localhost:8080/section-3/lesson-19-locators/app.html"
+    );
+
+    const importantText = page.getByText(
+      "This paragraph contains some important text that you might want to locate."
+    );
+    await expect(importantText).toBeVisible();
+  });
 });
