d31a18cf39e0f5802dbb23cf079d39485c0fece8
Pablo Cattaneo <pablocattaneo1@gmail.com>
2025-12-01 08:24:40 -0300

    Add tests for visibility of unique text identifiers and form control labels

--- tests/section-3/lesson-19-locators/lesson-19-locators.spec.ts
+++ tests/section-3/lesson-19-locators/lesson-19-locators.spec.ts
@@ -43,4 +43,48 @@ describe("Lesson 19: Locators: Locate elements by their text content.", () => {
     );
     await expect(importantText).toBeVisible();
   });
+  test("Special: Unique text identifier is visible", async ({ page }) => {
+    await page.goto(
+      "http://localhost:8080/section-3/lesson-19-locators/app.html"
+    );
+    const uniqueText = page.getByText("Special: Unique text identifier");
+    await expect(uniqueText).toBeVisible();
+  });
+  test("Click the button above to submit your information. is visible", async ({
+    page,
+  }) => {
+    await page.goto(
+      "http://localhost:8080/section-3/lesson-19-locators/app.html"
+    );
+    const buttonInstructionText = page.getByText(
+      "Click the button above to submit your information."
+    );
+    await expect(buttonInstructionText).toBeVisible();
+  });
+});
+
+describe("Lesson 19: Locators: Locate form controls by their associated label text.", () => {
+  test("Email Address: label is visible", async ({ page }) => {
+    await page.goto(
+      "http://localhost:8080/section-3/lesson-19-locators/app.html"
+    );
+
+    const emailLabel = page.getByLabel("Email Address:");
+    await expect(emailLabel).toBeVisible();
+  });
+  test("Password: label is visible", async ({ page }) => {
+    await page.goto(
+      "http://localhost:8080/section-3/lesson-19-locators/app.html"
+    );
+
+    const passwordLabel = page.getByLabel("Password:");
+    await expect(passwordLabel).toBeVisible();
+  });
+  test("Your Age: label is visible", async ({ page }) => {
+    await page.goto(
+      "http://localhost:8080/section-3/lesson-19-locators/app.html"
+    );
+    const ageLabel = page.getByLabel("Your Age:");
+    await expect(ageLabel).toBeVisible();
+  });
 });
