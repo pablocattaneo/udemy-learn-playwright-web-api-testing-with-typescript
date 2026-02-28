a4a43c1c3c494990d1ea3a1b635eee0e376d00ac
Pablo Cattaneo <pablocattaneo1@gmail.com>
2026-02-28 08:02:11 -0300

    Refactor single select dropdown tests to use getByRole for improved element selection

--- tests/singleSelectDropdown.spec.ts
+++ tests/singleSelectDropdown.spec.ts
@@ -4,12 +4,12 @@ const PAGE_URL = "https://testautomationpractice.blogspot.com/";
 
 test("Single select dropdown actions", async ({ page }) => {
   await page.goto(PAGE_URL);
-  const dropdown = page.locator("#country");
+  const dropdown = page.getByRole("combobox").and(page.locator("#country"));
   await dropdown.selectOption("India");
 });
 
 test("Single select dropdown actions select by value", async ({ page }) => {
     await page.goto(PAGE_URL);
-    const dropdown =page.locator("#country");
+    const dropdown = page.getByRole("combobox").and(page.locator("#country"));
     await dropdown.selectOption({ value: "uk" });
 });
