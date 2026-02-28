1381751f254f29e071638b8923f8569da817755f
Pablo Cattaneo <pablocattaneo1@gmail.com>
2026-02-28 08:23:24 -0300

    Add tests for counting options in single select dropdown and improve imports

--- tests/singleSelectDropdown.spec.ts
+++ tests/singleSelectDropdown.spec.ts
@@ -1,4 +1,5 @@
-import { test } from "@playwright/test";
+import { expect, test } from "@playwright/test";
+import { describe } from "node:test";
 
 const PAGE_URL = "https://testautomationpractice.blogspot.com/";
 
@@ -12,4 +13,12 @@ test("Single select dropdown actions select by value", async ({ page }) => {
     await page.goto(PAGE_URL);
     const dropdown = page.getByRole("combobox").and(page.locator("#country"));
     await dropdown.selectOption({ value: "uk" });
+});
+
+test("Counting the number of options in dropdown", async ({ page }) => {
+    await page.goto(PAGE_URL);
+    const dropdown = page.getByRole("combobox").and(page.locator("#country"));
+    const options = await dropdown.locator("option").all();
+    expect(options).toHaveLength(10);
+    console.log("Number of options in dropdown: " + options.length);
 });
