7170ec43b7c7cc5c6036428e39f096b5a7cf7641
Pablo Cattaneo <pablocattaneo1@gmail.com>
2026-02-28 08:38:53 -0300

    Rename tests for clarity and consistency; add test for alphabetical order of dropdown options

--- tests/singleSelectDropdown.spec.ts
+++ tests/singleSelectDropdown.spec.ts
@@ -3,7 +3,7 @@ import { describe } from "node:test";
 
 const PAGE_URL = "https://testautomationpractice.blogspot.com/";
 
-test("Single select dropdown actions", async ({ page }) => {
+test("Dropdown select dropdown actions", async ({ page }) => {
   await page.goto(PAGE_URL);
   const dropdown = page.getByRole("combobox").and(page.locator("#country"));
   await dropdown.selectOption("India");
@@ -15,10 +15,19 @@ test("Single select dropdown actions select by value", async ({ page }) => {
     await dropdown.selectOption({ value: "uk" });
 });
 
-test("Select should have 10 options", async ({ page }) => {
+test("Dropdown should have 10 options", async ({ page }) => {
     await page.goto(PAGE_URL);
     const dropdown = page.getByRole("combobox").and(page.locator("#country"));
     const options = await dropdown.locator("option").all();
     expect(options).toHaveLength(10);
     console.log("Number of options in dropdown: " + options.length);
+});
+
+test("Dropdown should be sorted in alphabetical order", async ({ page }) => {
+    await page.goto(PAGE_URL);
+    const dropdown = page.getByRole("combobox").and(page.locator("#country"));
+    const options = await dropdown.locator("option").all();
+    const optionValues = await Promise.all(options.map(option => option.textContent()));
+    const sortedOptionValues = [...optionValues].sort();
+    expect(optionValues).toEqual(sortedOptionValues);
 });
