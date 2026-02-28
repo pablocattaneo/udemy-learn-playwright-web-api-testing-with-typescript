29e0658774ee99071f99523cda1054506bfbcb61
Pablo Cattaneo <pablocattaneo1@gmail.com>
2026-02-28 07:59:53 -0300

    Add tests for single select dropdown actions and selection by value

---
+++ tests/singleSelectDropdown.spec.ts
@@ -0,0 +1,15 @@
+import { test } from "@playwright/test";
+
+const PAGE_URL = "https://testautomationpractice.blogspot.com/";
+
+test("Single select dropdown actions", async ({ page }) => {
+  await page.goto(PAGE_URL);
+  const dropdown = page.locator("#country");
+  await dropdown.selectOption("India");
+});
+
+test("Single select dropdown actions select by value", async ({ page }) => {
+    await page.goto(PAGE_URL);
+    const dropdown =page.locator("#country");
+    await dropdown.selectOption({ value: "uk" });
+});
