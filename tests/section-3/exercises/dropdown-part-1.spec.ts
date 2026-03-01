0642b79e7a7c9fb52e32d0eacfeec9e1f01ead1a
Pablo Cattaneo <pablocattaneo1@gmail.com>
2026-03-01 12:41:53 -0300

    Add test for sorting items by price in dropdown selection

---
+++ tests/section-3/exercises/dropdown-part-1.spec.ts
@@ -0,0 +1,21 @@
+import { test, expect } from "@playwright/test";
+
+const PAGE_URL = "https://www.bstackdemo.com/";
+const API_URL = "https://www.bstackdemo.com/api/products";
+
+test("Order By Lowest to Highest should sort items in descending order by price", async ({
+  page,
+}) => {
+  await page.goto(PAGE_URL);
+  const dropdown = page.getByRole("combobox");
+  await dropdown.selectOption({ value: "lowestprice" }, { timeout: 100000 });
+  await page.waitForResponse(response => 
+  response.url().includes(API_URL) && (response.status() === 200 || response.status() === 304)
+);
+  const prices = await page
+    .locator(".shelf-container .shelf-item .shelf-item__price .val b")
+    .allTextContents();
+  const priceValues = prices;
+  const sortedPriceValues = [...priceValues].sort((a, b) => a - b);
+  expect(priceValues).toEqual(sortedPriceValues);
+});
